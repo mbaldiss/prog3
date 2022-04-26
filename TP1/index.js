7e7b5bdfb9d0c6d101fa30ef606feaec4c1cc604
Marco Baldissone <marcol2c4@gmail.com>
2022-04-26 20:11:40 -0300

    TP1 done

---
+++ TP1/index.js
@@ -0,0 +1,14 @@
+// Trabajo practico 1
+// Baldissone Marco
+
+const numbersNames = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 999];
+
+let addition = 0;
+
+numbers.forEach(number => addition += number);
+
+const resultNumberNames = addition.toString().split("").map(number => numbersNames[number]);
+
+console.log(resultNumberNames);
